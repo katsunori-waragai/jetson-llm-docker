dc96f6358d12f62c0b8458e393e6fe3b06ecd406
katsunori-waragai <huyuhiko1128@gmail.com>
2024-04-19 23:46:27 +0900

    reformat by black

--- nanoowl-docker/wait_file.py
+++ nanoowl-docker/wait_file.py
@@ -4,8 +4,8 @@ import time
 found = False
 p = Path("/usr/lib/aarch64-linux-gnu/tegra/libnvdla_compiler.so")
 for i in range(100):
-    if p.stat().st_size >  0:
-       found = True
-       break
+    if p.stat().st_size > 0:
+        found = True
+        break
     time.sleep(1)
 print(f"{found=}")
--- yolov8-docker/segment_sample_imshow.py
+++ yolov8-docker/segment_sample_imshow.py
@@ -6,28 +6,29 @@ from pathlib import Path
 import os
 import cv2
 
+
 def read_result_image():
-	files = list(Path("runs/segment/").glob("predict*/*.jpg"))
-	files.sort(key=os.path.getmtime)
-	print(f"{files=}")
-	img = cv2.imread(str(files[-1]))
-	return img
+    files = list(Path("runs/segment/").glob("predict*/*.jpg"))
+    files.sort(key=os.path.getmtime)
+    print(f"{files=}")
+    img = cv2.imread(str(files[-1]))
+    return img
+
 
 from ultralytics import YOLO
+
 # モデルの生成。モデルは自動でダウンロードされます。
 model = YOLO("yolov8n-seg.pt")
 # 推論実行
 if 0:
-	src = "https://ultralytics.com/images/bus.jpg"
-	results = model(src, save=True)
+    src = "https://ultralytics.com/images/bus.jpg"
+    results = model(src, save=True)
 else:
-	capture = cv2.VideoCapture(0)
-	_, img = capture.read()
-	results = model(source=img, save=True) 
+    capture = cv2.VideoCapture(0)
+    _, img = capture.read()
+    results = model(source=img, save=True)
 
 img = read_result_image()
 print(f"{img=}")
 cv2.imshow("result", img)
 cv2.waitKey(-1)
-
-
