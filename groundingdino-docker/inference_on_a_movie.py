8dffa7b159c8ba6f3f44239e8aa0a0342eee1639
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-04-22 19:05:45 +0900

    Feature/capture (#17)
    why
    webcam 入力で検出を動作させたい。
    what
    prompt を変更
    cv2.VideoCaprure() を使うように変更した。

--- groundingdino-docker/inference_on_a_movie.py
+++ groundingdino-docker/inference_on_a_movie.py
@@ -14,6 +14,8 @@ from groundingdino.util.slconfig import SLConfig
 from groundingdino.util.utils import clean_state_dict, get_phrases_from_posmap
 from groundingdino.util.vl_utils import create_positive_map_from_span
 
+from inference_on_a_image import plot_boxes_to_image, get_grounding_output
+
 def pil2cv(image):
     ''' PIL型 -> OpenCV型 '''
     new_image = np.array(image, dtype=np.uint8)
@@ -25,50 +27,38 @@ def pil2cv(image):
         new_image = cv2.cvtColor(new_image, cv2.COLOR_RGBA2BGRA)
     return new_image
 
-def plot_boxes_to_image(image_pil, tgt):
-    H, W = tgt["size"]
-    boxes = tgt["boxes"]
-    labels = tgt["labels"]
-    assert len(boxes) == len(labels), "boxes and labels must have same length"
+def cv2pil(image):
+    ''' OpenCV型 -> PIL型 '''
+    new_image = image.copy()
+    if new_image.ndim == 2:  # モノクロ
+        pass
+    elif new_image.shape[2] == 3:  # カラー
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)
+    elif new_image.shape[2] == 4:  # 透過
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_BGRA2RGBA)
+    new_image = Image.fromarray(new_image)
+    return new_image
 
-    draw = ImageDraw.Draw(image_pil)
-    mask = Image.new("L", image_pil.size, 0)
-    mask_draw = ImageDraw.Draw(mask)
-
-    # draw boxes and masks
-    for box, label in zip(boxes, labels):
-        # from 0..1 to 0..W, 0..H
-        box = box * torch.Tensor([W, H, W, H])
-        # from xywh to xyxy
-        box[:2] -= box[2:] / 2
-        box[2:] += box[:2]
-        # random color
-        color = tuple(np.random.randint(0, 255, size=3).tolist())
-        # draw
-        x0, y0, x1, y1 = box
-        x0, y0, x1, y1 = int(x0), int(y0), int(x1), int(y1)
-
-        draw.rectangle([x0, y0, x1, y1], outline=color, width=6)
-        # draw.text((x0, y0), str(label), fill=color)
-
-        font = ImageFont.load_default()
-        if hasattr(font, "getbbox"):
-            bbox = draw.textbbox((x0, y0), str(label), font)
-        else:
-            w, h = draw.textsize(str(label), font)
-            bbox = (x0, y0, w + x0, y0 + h)
-        # bbox = draw.textbbox((x0, y0), str(label))
-        draw.rectangle(bbox, fill=color)
-        draw.text((x0, y0), str(label), fill="white")
-
-        mask_draw.rectangle([x0, y0, x1, y1], fill=255, width=6)
-
-    return image_pil, mask
 
 
 def load_image(image_path):
     # load image
     image_pil = Image.open(image_path).convert("RGB")  # load image
+
+    transform = T.Compose(
+        [
+            T.RandomResize([800], max_size=1333),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
+    image, _ = transform(image_pil, None)  # 3, h, w
+    return image_pil, image
+
+def capture_image(cap):
+    # load image
+    r, opencvimg = cap.read()
+    image_pil = cv2pil(opencvimg)
 
     transform = T.Compose(
         [
@@ -90,71 +80,6 @@ def load_model(model_config_path, model_checkpoint_path, cpu_only=False):
     print(load_res)
     _ = model.eval()
     return model
-
-
-def get_grounding_output(model, image, caption, box_threshold, text_threshold=None, with_logits=True, cpu_only=False, token_spans=None):
-    assert text_threshold is not None or token_spans is not None, "text_threshould and token_spans should not be None at the same time!"
-    caption = caption.lower()
-    caption = caption.strip()
-    if not caption.endswith("."):
-        caption = caption + "."
-    device = "cuda" if not cpu_only else "cpu"
-    model = model.to(device)
-    image = image.to(device)
-    with torch.no_grad():
-        outputs = model(image[None], captions=[caption])
-    logits = outputs["pred_logits"].sigmoid()[0]  # (nq, 256)
-    boxes = outputs["pred_boxes"][0]  # (nq, 4)
-
-    # filter output
-    if token_spans is None:
-        logits_filt = logits.cpu().clone()
-        boxes_filt = boxes.cpu().clone()
-        filt_mask = logits_filt.max(dim=1)[0] > box_threshold
-        logits_filt = logits_filt[filt_mask]  # num_filt, 256
-        boxes_filt = boxes_filt[filt_mask]  # num_filt, 4
-
-        # get phrase
-        tokenlizer = model.tokenizer
-        tokenized = tokenlizer(caption)
-        # build pred
-        pred_phrases = []
-        for logit, box in zip(logits_filt, boxes_filt):
-            pred_phrase = get_phrases_from_posmap(logit > text_threshold, tokenized, tokenlizer)
-            if with_logits:
-                pred_phrases.append(pred_phrase + f"({str(logit.max().item())[:4]})")
-            else:
-                pred_phrases.append(pred_phrase)
-    else:
-        # given-phrase mode
-        positive_maps = create_positive_map_from_span(
-            model.tokenizer(text_prompt),
-            token_span=token_spans
-        ).to(image.device) # n_phrase, 256
-
-        logits_for_phrases = positive_maps @ logits.T # n_phrase, nq
-        all_logits = []
-        all_phrases = []
-        all_boxes = []
-        for (token_span, logit_phr) in zip(token_spans, logits_for_phrases):
-            # get phrase
-            phrase = ' '.join([caption[_s:_e] for (_s, _e) in token_span])
-            # get mask
-            filt_mask = logit_phr > box_threshold
-            # filt box
-            all_boxes.append(boxes[filt_mask])
-            # filt logits
-            all_logits.append(logit_phr[filt_mask])
-            if with_logits:
-                logit_phr_num = logit_phr[filt_mask]
-                all_phrases.extend([phrase + f"({str(logit.item())[:4]})" for logit in logit_phr_num])
-            else:
-                all_phrases.extend([phrase for _ in range(len(filt_mask))])
-        boxes_filt = torch.cat(all_boxes, dim=0).cpu()
-        pred_phrases = all_phrases
-
-
-    return boxes_filt, pred_phrases
 
 
 if __name__ == "__main__":
@@ -195,32 +120,36 @@ if __name__ == "__main__":
     os.makedirs(output_dir, exist_ok=True)
     model = load_model(config_file, checkpoint_path, cpu_only=args.cpu_only)
 
-    image_pil, image = load_image(image_path)
-    # visualize raw image
-    image_pil.save(os.path.join(output_dir, "raw_image.jpg"))
+    cap = cv2.VideoCapture(0)
+    while True:
+        image_pil, image = capture_image(cap)
+        # visualize raw image
+        # image_pil.save(os.path.join(output_dir, "raw_image.jpg"))
 
-    # set the text_threshold to None if token_spans is set.
-    if token_spans is not None:
-        text_threshold = None
-        print("Using token_spans. Set the text_threshold to None.")
+        # set the text_threshold to None if token_spans is set.
+        if token_spans is not None:
+            text_threshold = None
+            print("Using token_spans. Set the text_threshold to None.")
 
 
-    # run model
-    boxes_filt, pred_phrases = get_grounding_output(
-        model, image, text_prompt, box_threshold, text_threshold, cpu_only=args.cpu_only, token_spans=eval(f"{token_spans}")
-    )
+        # run model
+        boxes_filt, pred_phrases = get_grounding_output(
+            model, image, text_prompt, box_threshold, text_threshold, cpu_only=args.cpu_only, token_spans=eval(f"{token_spans}")
+        )
 
-    # visualize pred
-    size = image_pil.size
-    pred_dict = {
-        "boxes": boxes_filt,
-        "size": [size[1], size[0]],  # H,W
-        "labels": pred_phrases,
-    }
-    # import ipdb; ipdb.set_trace()
-    image_with_box = plot_boxes_to_image(image_pil, pred_dict)[0]
-    image_with_box.save(os.path.join(output_dir, "pred.jpg"))
-    cvimg = pil2cv(image_with_box)
-    cv2.imshow("groundingDINO", cvimg)
-    cv2.waitKey(-1)
+        # visualize pred
+        size = image_pil.size
+        pred_dict = {
+            "boxes": boxes_filt,
+            "size": [size[1], size[0]],  # H,W
+            "labels": pred_phrases,
+        }
+        # import ipdb; ipdb.set_trace()
+        image_with_box = plot_boxes_to_image(image_pil, pred_dict)[0]
+        # image_with_box.save(os.path.join(output_dir, "pred.jpg"))
+        cvimg = pil2cv(image_with_box)
+        cv2.imshow("groundingDINO", cvimg)
+        key = cv2.waitKey(1)
+        if key == ord("q"):
+            break
 
