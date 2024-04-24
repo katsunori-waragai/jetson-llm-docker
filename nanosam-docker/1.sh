25d33dcfcaccedfade6a70ffac050db99ac67039
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-04-24 15:34:59 +0900

    note for download file destination

--- nanosam-docker/1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python3 -m pip install timm
-python3 -m pip install onnx
-mkdir -v data
-python3 -m nanosam.tools.export_sam_mask_decoder_onnx \
-    --model-type=vit_t \
-    --checkpoint=assets/mobile_sam.pt \
-    --output=data/mobile_sam_mask_decoder.onnx
-
--- nanosam-docker/2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:/usr/src/tensorrt/bin/
-
-trtexec \
-    --onnx=data/mobile_sam_mask_decoder.onnx \
-    --saveEngine=data/mobile_sam_mask_decoder.engine \
-    --minShapes=point_coords:1x1x2,point_labels:1x1 \
-    --optShapes=point_coords:1x1x2,point_labels:1x1 \
-    --maxShapes=point_coords:1x10x2,point_labels:1x10
