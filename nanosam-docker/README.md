a13b3dce81f4467005011aa9bfc80b6611cea62c
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-04-24 15:24:23 +0900

    already git clone done for nanosam

--- nanosam-docker/README.md
+++ nanosam-docker/README.md
@@ -4,4 +4,8 @@
 
 https://github.com/NVIDIA-AI-IOT/nanosam/blob/main/README.md
 
-## 
+## exmaple
+python3 examples/basic_usage.py \
+    --image_encoder=data/resnet18_image_encoder.engine \
+    --mask_decoder=data/mobile_sam_mask_decoder.engine
+
--- nanosam-docker/docker_run.sh
+++ nanosam-docker/docker_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export GIT_ROOT=$(cd $(dirname $0)/.. ; pwd)
 sudo docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY \
-	-v ${GIT_ROOT}/nanosam:/root/nanosam \
+        --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
 	--device /dev/bus/usb \
 	--device /dev/video0:/dev/video0:mwr \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix nanosam:100
