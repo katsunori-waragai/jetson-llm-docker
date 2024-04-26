2a157d310d31f65554e71c05e2eec0bc9b6a76d6
katsunori-waragai <huyuhiko1128@gmail.com>
2024-04-26 18:02:07 +0900

    check only

--- nanosam-docker/default-runtime.py
+++ nanosam-docker/default-runtime.py
@@ -4,29 +4,28 @@ import os
 import inspect
 import shutil
 
-file = Path("/etc/docker/daemon.json")
-d = json.loads(file.open("rt").read())
-print(d)
-if d.get("default-runtime", None) == "nvidia":
-  exit(0)  # OK
+if __name__ == "__main__":
+    file = Path("/etc/docker/daemon.json")
+    d = json.loads(file.open("rt").read())
+    print(d)
+    if d.get("default-runtime", None) == "nvidia":
+      exit(0)  # OK
 
+    message = f"""Be sure to add following line in {str(file)}
+    "default-runtime": "nvidia",
+    """
+    print(message)
+    exit(1)
 
-message = f"""Be sure to add following line in {str(file)}
-"default-runtime": "nvidia",
-"""
-
-print(message)
-exit(1)
-
-# if os.access(file, os.W_OK):
-#   # make dated backup
-#   backupname = file.dirname() / f"{file.name}_back"
-#   shutil.copy(str(file), str(backupname))
-#
-# print(f"{file.stat().st_mode=}")
-# print(f"{file.is_writable()=}")
-#
-# d["default-runtime"] = "nvidia"
-# newfile = Path(file.name)
-# newfile.open("wt").write(json.dumps(d, sort_keys=True, indent=4))
-# print("generated new daemon.json")
+    # if os.access(file, os.W_OK):
+    #   # make dated backup
+    #   backupname = file.dirname() / f"{file.name}_back"
+    #   shutil.copy(str(file), str(backupname))
+    #
+    # print(f"{file.stat().st_mode=}")
+    # print(f"{file.is_writable()=}")
+    #
+    # d["default-runtime"] = "nvidia"
+    # newfile = Path(file.name)
+    # newfile.open("wt").write(json.dumps(d, sort_keys=True, indent=4))
+    # print("generated new daemon.json")
