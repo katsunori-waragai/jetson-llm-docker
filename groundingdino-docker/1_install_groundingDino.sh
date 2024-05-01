a93e5c2e63c860ac49755d67e9b0101f1b1507ac
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-05-01 17:19:17 +0900

    Feature/dino again docker (#41)
    why
    weights を毎回download してしまう。
    what
    weights の7download をDockerfile の中で実行するようにした。

--- groundingdino-docker/1_install_groundingDino.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd /root/GroundingDINO
-python3 -m pip install -e .
-
-python3 -m pip uninstall -y opencv-python
-python3 -m pip uninstall -y opencv-python-headless
-python3 -m pip install opencv-python==3.4.18.65
-
