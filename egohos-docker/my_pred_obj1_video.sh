19a50d982c7ccd6997ac21ba3edf8a2446caff63
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-05-08 13:33:01 +0900

    added my_pred_obj1_video.sh

---
+++ egohos-docker/my_pred_obj1_video.sh
@@ -0,0 +1,5 @@
+python3 predict_video.py \
+       --mode obj1 \
+       --input_video_file ../my_testvideos/ego_cup_book.mp4 \
+       --output_video_file ../my_testvideos/ego_cup_book_result.mp4 \
+       --remove_intermediate_images True # True: save only the processed video; False: save all intermediate segmentation image results
