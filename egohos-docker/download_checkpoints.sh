06fa165a4fd0024270b60b0bd054fb8c2893084a
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-05-13 16:37:32 +0900

    download only if file was not found (#48)

--- egohos-docker/download_checkpoints.sh
+++ egohos-docker/download_checkpoints.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 python3 -m pip install gdown
 cd mmsegmentation
-gdown --fuzzy -O work_dirs.zip https://drive.google.com/file/d/1DEJBeQ3cR1q7cjjzwDUIQVSoptT-y9U7/view?usp=drive_link
+if [ ! -f work_dirs.zip ]; then
+  gdown --fuzzy -O work_dirs.zip https://drive.google.com/file/d/1DEJBeQ3cR1q7cjjzwDUIQVSoptT-y9U7/view?usp=drive_link
+fi
 unzip work_dirs.zip
 # rm work_dirs.zip
