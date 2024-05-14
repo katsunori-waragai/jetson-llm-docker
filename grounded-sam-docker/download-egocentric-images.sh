5875547811eb9cdf250b5284880d4b2433199801
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-05-14 18:09:05 +0900

    fix gdown use

--- grounded-sam-docker/download-egocentric-images.sh
+++ grounded-sam-docker/download-egocentric-images.sh
@@ -1,4 +1,5 @@
-!/bin/bash
+#!/bin/bash
 cd /root/data/
-gdown --fuzzy https://drive.google.com/drive/folders/14DgHOvSSjjs46cED6kng7k4y5q7Qdoeb?usp=drive_link
+# mkdir -p data && cd data
+gdown --fuzzy --folder https://drive.google.com/drive/folders/14DgHOvSSjjs46cED6kng7k4y5q7Qdoeb?usp=drive_link
 
