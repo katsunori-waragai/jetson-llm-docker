d18b798518922ccecd3fab82607b73661ad21bd7
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-05-15 12:54:16 +0900

    Feature/grounded sam etc (#51)
    why
    grounded-sam-docker/grounded_sam_demo.py がこなれていない。
    what
    grounded-sam-docker/grounded_sam_demo_my.py を開発用に追加した。
    関数の抽出
    実行順序を変更し、画像読み込み以降の処理をスクリプトの後半にした。

---
+++ grounded-sam-docker/grounded_sam_demo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+case $# in
+  2)
+    name=$1
+    prompt=$2;;
+  *)
+    echo "usage:$0 name text_prompt"
+    exit 1;;
+esac
+
+if [ ! -f ${name} ] ; then
+   echo "not found ${name}"
+   exit
+fi
+  
+echo python3 grounded_sam_demo.py   --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py \
+   --grounded_checkpoint groundingdino_swint_ogc.pth \
+   --sam_checkpoint sam_vit_h_4b8939.pth \
+   --input_image ${name} \
+   --output_dir "outputs" \
+   --box_threshold 0.3   --text_threshold 0.25 \
+   --text_prompt "${prompt}" \
+   --device "cuda"
+
