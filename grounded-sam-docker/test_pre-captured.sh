82f0b8f8886761d151afdc9e26140a46e29d7a24
katsunori-waragai <huyuhiko1128@gmail.com>
2024-05-17 17:04:23 +0900

    add test_pre-captured.sh

--- grounded-sam-docker/test_pre-captured.sh
+++ grounded-sam-docker/test_pre-captured.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-python3 cap.py --is_zed
+gdown --fuzzy --folder https://drive.google.com/drive/folders/1L1ZZPjTvswFxyNE5K75lAmMi-znHzfNx?usp=sharing
 python3 grounded_sam_demo_my.py   --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py \
    --grounded_checkpoint groundingdino_swint_ogc.pth \
    --sam_checkpoint sam_vit_h_4b8939.pth \
-   --image_dir captured \
+   --image_dir captured_20240517 \
    --output_dir "outputs_captured" \
    --box_threshold 0.3   --text_threshold 0.25 \
    --text_prompt "arm . cup . keyboard . table " \
