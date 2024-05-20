0277acd97472a91a42071812de9a2c390760a3a7
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-05-20 10:43:02 +0900

    added tempory test script

---
+++ grounded-sam-docker/test_pre-captured_movie.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export CAPTURED_FOLDER=captured_20240517
+if [ ! -d ${CAPTURED_FOLDER} ]; then
+  gdown --fuzzy --folder https://drive.google.com/drive/folders/1L1ZZPjTvswFxyNE5K75lAmMi-znHzfNx?usp=sharing
+else
+  echo already downloaded ${CAPTURED_FOLDER}
+  echo ${CAPTURED_FOLDER}/*.jpg | sed 's/ /\n/g'
+fi
+echo "start grounded Dino"
+python3 grounded_sam_movie.py   --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py \
+   --grounded_checkpoint groundingdino_swint_ogc.pth \
+   --sam_checkpoint sam_vit_h_4b8939.pth \
+   --image_dir ${CAPTURED_FOLDER} \
+   --output_dir "outputs_captured" \
+   --box_threshold 0.3   --text_threshold 0.25 \
+   --text_prompt "arm . cup . keyboard . table " \
+   --device "cuda"
