c921e61ea4c05d7b5e03093856baa3e024b9e0b3
katsunori-waragai <huyuhiko1128@gmail.com>
2024-05-20 17:45:44 +0900

    added more test_prompt

--- grounded-sam-docker/run_usbcam.sh
+++ grounded-sam-docker/run_usbcam.sh
@@ -5,5 +5,5 @@ python3 grounded_sam_movie.py   --config GroundingDINO/groundingdino/config/Grou
    --sam_checkpoint sam_vit_h_4b8939.pth \
    --output_dir "outputs_captured" \
    --box_threshold 0.3   --text_threshold 0.25 \
-   --text_prompt "arm . cup . keyboard . table . plate . bottle . PC " \
+   --text_prompt "arm . cup . keyboard . table . plate . bottle . PC . person" \
    --device "cuda"
