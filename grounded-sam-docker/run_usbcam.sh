20c032d86385706735987792b3e3a6966862d94f
katsunori-waragai <huyuhiko1128@gmail.com>
2024-05-20 11:26:28 +0900

    removed --image_dir

--- grounded-sam-docker/run_usbcam.sh
+++ grounded-sam-docker/run_usbcam.sh
@@ -3,7 +3,6 @@ echo "start grounded Dino"
 python3 grounded_sam_movie.py   --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py \
    --grounded_checkpoint groundingdino_swint_ogc.pth \
    --sam_checkpoint sam_vit_h_4b8939.pth \
-   --image_dir ${CAPTURED_FOLDER} \
    --output_dir "outputs_captured" \
    --box_threshold 0.3   --text_threshold 0.25 \
    --text_prompt "arm . cup . keyboard . table " \
