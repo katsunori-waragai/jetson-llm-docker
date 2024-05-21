8745b9bab43c06d019edd5b6cbd2dcd158d029c1
katsunori-waragai <huyuhiko1128@gmail.com>
2024-05-21 17:24:52 +0900

    simplify

--- grounded-sam-docker/grounded_sam_demo_my.py
+++ grounded-sam-docker/grounded_sam_demo_my.py
@@ -225,7 +225,7 @@ class GroundedSAMPredictor:
         used = {}
         image_pil = cv2pil(cvimage)
         H, W = cvimage.shape[:2]
-        torch_image, _ = transform(image_pil, None)  # 3, h, w
+        torch_image, _ = self.transform(image_pil, None)  # 3, h, w
         # Dinoによる検出
         t0 = cv2.getTickCount()
         boxes_filt, pred_phrases = get_grounding_output(
