dac43255477077f671adcc509887f6f1fd51c06c
katsunori-waragai <huyuhiko1128@gmail.com>
2024-05-23 17:35:59 +0900

    use python3

--- grounded-sam-docker/run_hq.sh
+++ grounded-sam-docker/run_hq.sh
@@ -2,7 +2,7 @@
 wget https://github.com/IDEA-Research/detrex-storage/releases/download/grounded-sam-storage/sam_hq_demo_image.png
 
 export CUDA_VISIBLE_DEVICES=0
-python grounded_sam_demo.py \
+python3 grounded_sam_demo.py \
   --config GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py \
   --grounded_checkpoint groundingdino_swint_ogc.pth \
   --sam_hq_checkpoint ./sam_hq_vit_h.pth \
