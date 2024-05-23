b133ae0c940874c6b16d8f78fb368dab12fd3d32
katsunori-waragai <huyuhiko1128@gmail.com>
2024-05-23 18:04:54 +0900

    chaged some output dir

--- grounded-sam-docker/test_hq.sh
+++ grounded-sam-docker/test_hq.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-mkdir -p image_hq
-cd image_hq && wget https://github.com/IDEA-Research/detrex-storage/releases/download/grounded-sam-storage/sam_hq_demo_image.png
+IMAGE_HQ_DIR=image_hq
+mkdir -p ${IMAGE_HQ_DIR}
+cd ${IMAGE_HQ_DIR} && wget https://github.com/IDEA-Research/detrex-storage/releases/download/grounded-sam-storage/sam_hq_demo_image.png
 cd ..
 
 export CUDA_VISIBLE_DEVICES=0
 python3 gsam.py \
    --use_sam_hq \
-   --image_dir image_hq \
-   --output_dir "outputs_image_hq" \
+   --image_dir ${IMAGE_HQ_DIR} \
+   --output_dir "outputs_${IMAGE_HQ_DIR}" \
    --box_threshold 0.3   --text_threshold 0.25 \
    --text_prompt "arm . cup . keyboard . table . chair" \
